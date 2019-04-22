14f8aedf5c2b4f8aa6980c7ee32776e96ebd2efb
Dalmo <dalmo@dalmopc.ua01.trtsp.jus.br>
2019-04-22 18:37:14 -0300

    quarto commit

--- alurapic/cliente/src/app/app.routing.module.ts
+++ alurapic/cliente/src/app/app.routing.module.ts
@@ -4,8 +4,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { TesteComponent } from './testes/teste/teste/teste.component';
 
 const routes: Routes = [
+    { path: 'teste', component: TesteComponent },
     { path: 'user/:userName', component: PhotoListComponent },
     { path: 'p/add', component: PhotoFormComponent },
     { path: '**', component: NotFoundComponent }  
