03155fe10d3547804cd31be45434012dafc09a60
Dalmo <dalmo@dalmopc.ua01.trtsp.jus.br>
2019-04-23 12:56:12 -0300

    outro

--- alurapic/cliente/src/app/testes/teste/teste.module.ts
+++ alurapic/cliente/src/app/testes/teste/teste.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TesteComponent } from './teste/teste.component';
+import { ObjetoComponent } from './objeto/objeto.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TesteComponent]
+  declarations: [TesteComponent, ObjetoComponent]
 })
 export class TesteModule { }
